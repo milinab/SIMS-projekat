26ba39d81d59004b7c750beda276742f9dea95f1
unknown <nemkejelic15@gmail.com>
2022-04-10 23:43:22 +0200

    enums created

--- Hospital/Hospital/Model/Specialist.cs
+++ Hospital/Hospital/Model/Specialist.cs
@@ -2,6 +2,29 @@ using System;
 
 namespace Hospital.Model
 {
+    public enum SpecializationType
+    {
+        AllergyAndImmunology,
+        Anesthesiology,
+        Dermatology,
+        DiagnosticRadiology,
+        EmergencyMedicine,
+        FamilyMedicine,
+        InternalMedicine,
+        MedicalGenetics,
+        Neurology,
+        NuclearMedicine,
+        ObstetricsAndGynecology,
+        Ophthalmology,
+        Pathology,
+        Pediatrics,
+        PhysicalMedicineAndRehabilitation,
+        PreventiveMedicine,
+        Psychiatry,
+        RadiationOncology,
+        Surgery,
+        Urology
+    }
    public class Specialist : Doctor
    {
       public SpecializationType Specialization;
