e6ed02a15e44adf375c43aa50fc63b4e48e5ef23
Marko Uljarevic <uljarevic.ra233.2019@uns.ac.rs>
2022-04-27 16:45:35 +0200

    added doctor menu user control

---
+++ Hospital/Hospital/Doctor/Menu.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hospital.Doctor
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Checkup(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Appointments(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Patients(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Requests(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MalfunctionReport(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Account(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogOut(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
