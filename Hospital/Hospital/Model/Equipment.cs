37904e2f3fcf6df1e0cd488582bb4ee7ae1d51e5
unknown <nemkejelic15@gmail.com>
2022-05-03 23:07:48 +0200

    relocation implemented

--- Hospital/Hospital/Model/Equipment.cs
+++ Hospital/Hospital/Model/Equipment.cs
@@ -3,9 +3,9 @@ using System.ComponentModel;
 using System.Runtime.Serialization;
 
 namespace Hospital.Model
-{ 
+{
     [DataContract]
-    public class Equipment 
+    public class Equipment
     {
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string name)
@@ -17,7 +17,7 @@ namespace Hospital.Model
         }
 
         private string _id;
-        private int _number;
+        private string _number;
         private string _name;
         private string _room;
 
@@ -25,7 +25,8 @@ namespace Hospital.Model
         public string Id
         {
             get { return _id; }
-            set {
+            set
+            {
                 if (value != _id)
                 {
                     _id = value;
@@ -37,13 +38,21 @@ namespace Hospital.Model
         public string Room
         {
             get { return _room; }
-            set { _room = value; }
+            set { 
+                if(value!= _room)
+                {
+                    _room = value;
+                    OnPropertyChanged("Room");
+                }
+                _room = value;
+            }
         }
         [DataMember]
-        public int Number
+        public string Number
         {
             get { return _number; }
-            set {
+            set
+            {
                 if (value != _number)
                 {
                     _number = value;
@@ -55,29 +64,47 @@ namespace Hospital.Model
         public string Name
         {
             get { return _name; }
-            set { if (value != _name)
+            set
+            {
+                if (value != _name)
                 {
                     _name = value;
                     OnPropertyChanged("Name");
                 }
             }
         }
+        //[DataMember]
+        //public BindingList<int> QuantityByRoom;
+
+        //[DataMember]
+        //public BindingList<Room> rooms;
+
+
         public Equipment()
         {
 
         }
-        public Equipment(string id, int num, string name)
+        public Equipment(string id, string num, string name)
         {
             Id = id;
             Number = num;
             Name = name;
         }
-        public Equipment(string id, int num, string name, string room)
+        public Equipment(string id, string num, string name, string room)
         {
             Id = id;
             Number = num;
             Name = name;
             Room = room;
         }
+
+        /*public Equipment(string name, string num, BindingList<int> quantityByRoom, BindingList<Room> rooms)
+        {
+            Name = name;
+            Number = num;
+            this.QuantityByRoom = quantityByRoom;
+            this.rooms = rooms;
+        }*/
+
     }
 }
