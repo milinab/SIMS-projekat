613957a9e56d479b2faa78c8184dee405ec0a793
Marko Uljarevic <uljarevic.ra233.2019@uns.ac.rs>
2022-05-04 19:12:05 +0200

    added appointment validation

--- Hospital/Hospital/Service/AppointmentService.cs
+++ Hospital/Hospital/Service/AppointmentService.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using Hospital.Model;
 using Hospital.Repository;
+using Xceed.Wpf.Toolkit;
 
 namespace Hospital.Service
 {
@@ -32,14 +33,30 @@ namespace Hospital.Service
 
         public void Create(Appointment newAppointment)
         {
-            // var appointments = Read();
-            // foreach (var appointment in appointments)
-            // {
-            //     var startTime = newAppointment.Date;
-            //     var tempTime = startTime;
-            //     var endTime = tempTime.AddMinutes(newAppointment.Duration.Hours / 60 + newAppointment.Duration.Minutes);
-            //     
-            // }
+            var appointments = Read();
+            foreach (var appointment in appointments)
+            {
+                var startTime = appointment.Date;
+                var tempTime = startTime;
+                var endTime = tempTime.AddMinutes(appointment.Duration.Hours * 60 + appointment.Duration.Minutes);
+
+                var appointmentStarTime = newAppointment.Date;
+                var appointmentTempTime = appointmentStarTime;
+                var appointmentEndTime = appointmentTempTime.AddMinutes(newAppointment.Duration.Hours * 60 +
+                                                                        newAppointment.Duration.Minutes);
+                
+                if (DateTime.Compare(newAppointment.Date, startTime) > 0 && DateTime.Compare(newAppointment.Date, endTime) < 0)
+                {
+                    MessageBox.Show("There is already an appointment at the selected time!");
+                    return;
+                }
+
+                if (DateTime.Compare(appointmentEndTime, appointment.Date) > 0 && DateTime.Compare(appointmentEndTime, endTime) < 0)
+                {
+                    MessageBox.Show("There is already an appointment at the selected time!");
+                    return;
+                }
+            }
             newAppointment.Id = GenerateId();
             _repository.Create(newAppointment);
         }
