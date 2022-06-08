0274caa04b42d04c9d869143ad18e6efb1312ddc
Milina Bucko <milina.bph@gmail.com>
2022-06-08 12:09:18 +0200

    added Graphics view, started with mvvm

---
+++ Hospital/Hospital/View/PatientView/Graphics.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Hospital.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hospital.View.PatientView
+{
+    /// <summary>
+    /// Interaction logic for Graphics.xaml
+    /// </summary>
+    public partial class Graphics : Page
+    {
+        private App app;
+        private readonly object _content;
+        private readonly PatientWindow _patientWindow;
+        public Patient patient;
+        public Graphics(PatientWindow patientWindow)
+        {
+            InitializeComponent();
+            app = Application.Current as App;
+            _content = Content;
+            this.DataContext = this;
+            _patientWindow = patientWindow;
+            patient = app._patientController.ReadById(LogIn.LoggedUser.Id);
+        }
+        private void HomePage_Click(object sender, RoutedEventArgs e)
+        {
+            Page homePage = new HomePage(_patientWindow);
+            this.frame.Navigate(homePage);
+        }
+
+        private void Profile_Click(object sender, RoutedEventArgs e)
+        {
+            Page profilePage = new Profile(_patientWindow);
+            this.frame.Navigate(profilePage);
+        }
+
+        private void MedicalRecord_Click(object sender, RoutedEventArgs e)
+        {
+            User user = app._userController.ReadById(patient.Id);
+            Address address = app._addressController.ReadById(user.Address.Id);
+            City city = app._cityController.ReadById(user.Address.CityId);
+            Country country = app._countryController.ReadById(1); //country nije postavljen u address modelu
+            Model.MedicalRecord medicalRecord = app._medicalRecordController.ReadById(patient.MedicalRecordId);
+            ObservableCollection<Allergen> allergens = app._allergenController.ReadByIds(medicalRecord.AllergenIds);
+
+            Page medicalRecordPage = new MedicalRecord(_patientWindow, user, patient, address, city, country, medicalRecord, allergens);
+            this.frame.Navigate(medicalRecordPage);
+        }
+
+        private void MyAppointments_Click(object sender, RoutedEventArgs e)
+        {
+            _patientWindow.BackToPatientWindow();
+        }
+        private void MyTherapy_Click(object sender, RoutedEventArgs e)
+        {
+            Page myTherapyPage = new MyTherapy(_patientWindow);
+            this.frame.Navigate(myTherapyPage);
+        }
+
+        private void Calendar_Click(object sender, RoutedEventArgs e)
+        {
+            Page calendarPage = new Calendar(_patientWindow);
+            this.frame.Navigate(calendarPage);
+        }
+        private void Notes_Click(object sender, RoutedEventArgs e)
+        {
+            Page notesPage = new Notes(_patientWindow);
+            this.frame.Navigate(notesPage);
+        }
+
+        private void Surveys_Click(object sender, RoutedEventArgs e)
+        {
+            Page hospitalSurveyPage = new Surveys(_patientWindow);
+            this.frame.Navigate(hospitalSurveyPage);
+        }
+        private void Notification_Click(object sender, RoutedEventArgs e)
+        {
+            Page notificationPage = new Notification(_patientWindow);
+            this.frame.Navigate(notificationPage);
+        }
+
+        public void BackToPatientWindow()
+        {
+            Content = _content;
+        }
+
+        private void LogOut_Click(object sender, RoutedEventArgs e)
+        {
+            LogIn logIn = new LogIn();
+            logIn.Show();
+            _patientWindow.Close();
+        }
+    }
+}
