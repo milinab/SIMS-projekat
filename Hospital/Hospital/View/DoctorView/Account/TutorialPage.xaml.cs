53e312aadc7d97a234421aed0834ebd32223ef29
Marko Uljarevic <uljarevic.ra233.2019@uns.ac.rs>
2022-06-16 02:15:06 +0200

    tutorial path changed

--- Hospital/Hospital/View/DoctorView/Account/TutorialPage.xaml.cs
+++ Hospital/Hospital/View/DoctorView/Account/TutorialPage.xaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -8,6 +10,11 @@ namespace Hospital.View.DoctorView.Account
         public TutorialPage()
         {
             InitializeComponent();
+            var sep = Path.DirectorySeparatorChar;
+            var path = AppContext.BaseDirectory + ".." + sep + ".." + sep + "View" + sep +
+                       "DoctorView" + sep + "Resources" + sep + "tutorial.mp4";
+            MessageBox.Show(path);
+            VideoMediaElement.Source = new Uri(path);
         }
 
         private void Back_OnClick(object sender, RoutedEventArgs e)
